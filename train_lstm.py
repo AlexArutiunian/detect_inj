2509ab4794b3da5c6d5b1f1b45bbd3d101e77c2b
dieuvinakilebe <dieuvinantangou@gmail.com>
2025-08-18 20:50:51 +0300

    add lstm

--- train_lstm.py
+++ train_lstm.py
@@ -405,7 +405,7 @@ def main():
 
     # Коллбеки
     cb = [
-        tf.keras.callbacks.EarlyStopping(monitor="val_loss", patience=5, restore_best_weights=True),
+        tf.keras.callbacks.EarlyStopping(monitor="val_loss", patience=12, restore_best_weights=True),
         tf.keras.callbacks.ReduceLROnPlateau(monitor="val_loss", factor=0.5, patience=2, min_lr=1e-6)
     ]
 
